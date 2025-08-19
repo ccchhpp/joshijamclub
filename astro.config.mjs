d4dcd4f5368364e226c7da83a9c97964f60dd108
tailofleaves <tailofleaves@gmail.com>
2025-08-18 23:59:26 -0700

    updated contact form implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,7 @@ export default defineConfig({
   site: 'https://joshijam.club', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true, // Generate sitemap (set to "false" to disable)
   integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
+  security: {
+    checkOrigin: false
+  }
 });
